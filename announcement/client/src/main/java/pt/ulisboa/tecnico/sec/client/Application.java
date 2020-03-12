2116d0bc534e485f7e026d9cac5237dc36f9106e
danielaLopes <danielalopes_97@hotmail.com>
2020-03-12 10:28:39 +0000

    project setup with maven

---
+++ announcement/client/src/main/java/pt/ulisboa/tecnico/sec/client/Application.java
@@ -0,0 +1,9 @@
+package pt.ulisboa.tecnico.sec.client;
+
+
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world client");
+    }
+}
---
+++ announcement/server/src/main/java/pt/ulisboa/tecnico/sec/server/Application.java
@@ -0,0 +1,7 @@
+package pt.ulisboa.tecnico.sec.server;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world server");
+    }
+}
