8cac28d8df03f4b1e381d029486d39e428e061cb
facasaca <franciscoccasaca@gmail.com>
2020-03-19 15:15:31 +0000

    Client generates UUID when posting an announcement

--- announcement/client/src/main/java/pt/ulisboa/tecnico/sec/client/Client.java
+++ announcement/client/src/main/java/pt/ulisboa/tecnico/sec/client/Client.java
@@ -2,6 +2,7 @@ package pt.ulisboa.tecnico.sec.client;
 
 import pt.ulisboa.tecnico.sec.crypto_lib.KeyGenerator;
 import pt.ulisboa.tecnico.sec.communication_lib.Communication;
+import pt.ulisboa.tecnico.sec.crypto_lib.UUIDGenerator;
 
 import java.security.KeyPair;
 import java.security.PublicKey;
@@ -14,10 +15,12 @@ public class Client {
     private final PublicKey _pubKey;
     private final Communication _communication;
     private Socket _clientSocket;
+    private UUIDGenerator _uuidGenerator;
 
     public Client() {
         _pubKey = generateKeyPair(false);
         _communication = new Communication();
+        _uuidGenerator = new UUIDGenerator();
     }
 
     // TODO: move method to shared library "crypto_lib"
@@ -58,21 +61,25 @@ public class Client {
 
     /**
      * Posts an announcement to the Client's Board. This announcement
-     * can refer to previous announcements.
+     * can refer to previous announcements and has a UUID.
      * @param message to be announced
      * @param reference to previous announcements
      */
     public void post(String message, String reference) {
+        int uuid = _uuidGenerator.generateUUID();
+        System.out.println("Posting announcement to user's Board with uuid: " + uuid);
         // TODO: client-server communication
     }
 
     /**
      * Posts an announcement to the General Board. This announcement
-     * can refer to previous announcements.
+     * can refer to previous announcements and has a UUID.
      * @param message to be announced
      * @param reference to previous announcements
      */
     public void postGeneral(String message, String reference) {
+        int uuid = _uuidGenerator.generateUUID();
+        System.out.println("Posting announcement to General Board with uuid: " + uuid);
         // TODO: client-server communication
     }
 
---
+++ announcement/crypto_lib/src/main/java/pt/ulisboa/tecnico/sec/crypto_lib/UUIDGenerator.java
@@ -0,0 +1,27 @@
+package pt.ulisboa.tecnico.sec.crypto_lib;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.util.UUID;
+
+public class UUIDGenerator {
+
+    public int generateUUID() {
+        String uuid = UUID.randomUUID().toString();
+        String time = Instant.now().toString();
+        String announcementId = uuid + "-" + time;
+        int encodedAnnouncementId = -1;
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            encodedAnnouncementId = digest.digest(
+                    announcementId.getBytes(StandardCharsets.UTF_8)).hashCode();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Wrong algorithm to generate message digest");
+        }
+
+        return encodedAnnouncementId;
+    }
+}
