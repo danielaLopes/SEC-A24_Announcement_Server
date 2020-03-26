85ab65bbb23540377f769f69134a51762932dd4c
facasaca <franciscoccasaca@gmail.com>
2020-03-26 14:26:06 +0000

    Generate 3 Client key pairs

--- announcement/generateClientKeys.sh
+++ announcement/generateClientKeys.sh
@@ -1,7 +1,25 @@
-openssl genrsa -out client/src/main/resources/crypto/client.key
+openssl genrsa -out client/src/main/resources/crypto/client1.key
 
-openssl rsa -in client/src/main/resources/crypto/client.key -out client/src/main/resources/crypto/public.key -pubout
+openssl rsa -in client/src/main/resources/crypto/client1.key -out client/src/main/resources/crypto/public1.key -pubout
 
-openssl req -new -key client/src/main/resources/crypto/client.key -out client/src/main/resources/crypto/client.csr
+openssl req -new -key client/src/main/resources/crypto/client1.key -out client/src/main/resources/crypto/client1.csr
 
-openssl x509 -req -days 365 -in client/src/main/resources/crypto/client.csr -CA server/src/main/resources/crypto/server.crt -CAkey server/src/main/resources/crypto/server.key -out client/src/main/resources/crypto/client.crt
+openssl x509 -req -days 365 -in client/src/main/resources/crypto/client1.csr -CA server/src/main/resources/crypto/server.crt -CAkey server/src/main/resources/crypto/server.key -out client/src/main/resources/crypto/client1.crt
+
+
+openssl genrsa -out client/src/main/resources/crypto/client2.key
+
+openssl rsa -in client/src/main/resources/crypto/client2.key -out client/src/main/resources/crypto/public2.key -pubout
+
+openssl req -new -key client/src/main/resources/crypto/client2.key -out client/src/main/resources/crypto/client2.csr
+
+openssl x509 -req -days 365 -in client/src/main/resources/crypto/client2.csr -CA server/src/main/resources/crypto/server.crt -CAkey server/src/main/resources/crypto/server.key -out client/src/main/resources/crypto/client2.crt
+
+
+openssl genrsa -out client/src/main/resources/crypto/client3.key
+
+openssl rsa -in client/src/main/resources/crypto/client3.key -out client/src/main/resources/crypto/public3.key -pubout
+
+openssl req -new -key client/src/main/resources/crypto/client3.key -out client/src/main/resources/crypto/client3.csr
+
+openssl x509 -req -days 365 -in client/src/main/resources/crypto/client3.csr -CA server/src/main/resources/crypto/server.crt -CAkey server/src/main/resources/crypto/server.key -out client/src/main/resources/crypto/client3.crt
